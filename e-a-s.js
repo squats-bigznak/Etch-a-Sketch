d5b6e08d4d01b00980f99cbbceea8edff2188502
squats-bigznak <88125284+squats-bigznak@users.noreply.github.com>
2023-03-15 01:31:36 -0700

    Implemented color increment

--- e-a-s.js
+++ e-a-s.js
@@ -1,28 +1,34 @@
 
 const gridContainer = document.querySelector('.gridContainer');
 
-let dimensions = 50;
+let dimensions = 100;
 let ps = [];
 const stretch = parseInt(960/dimensions);
 gridContainer.style.gridTemplateColumns = (`repeat(${dimensions}, 1fr`);
 gridContainer.style.gridTemplateRows = (`repeat(${dimensions}, 1fr`);
-var colors = ['#000', '#111', '#222', '#333', '#444', '#555',
-                '#666', '#777', '#888'];
-let picker = 0
+let colors = ['#000', '#111', '#222', '#333', '#444', '#555',
+                '#666', '#777', '#888', '#999', '#AAA', '#BBB',
+                '#CCC', '#DDD', '#EEE', '#FFF'];
+let picker = new Array(dimensions * dimensions).fill(0);
+
+
 
 for (let i = 0; i < (dimensions*dimensions); i++){
-
+        
         ps[i] = document.createElement('div');
         ps[i].classList.add('ps');
         ps[i].style.width = stretch+'px';
         ps[i].style.height = stretch+'px'; 
         ps[i].addEventListener('mouseover', function(event){
-            
-            
-            
-            event.target.style.backgroundColor = colors[picker%9];
-            ++picker;
+             
+            event.target.style.backgroundColor = colors[(picker[i] += 1)%16];
+            // ++picker;
         })
+        // ps[i].addEventListener('mouseenter', function(event){
+            
+        //     event.target.style.backgroundColor = colors[0];
+        //     // ++picker;
+        // })
         
         gridContainer.appendChild(ps[i]);
         

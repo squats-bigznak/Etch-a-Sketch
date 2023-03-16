c8a80e356f9e1a79b14e17afd78e987f64d95ea8
squats-bigznak <88125284+squats-bigznak@users.noreply.github.com>
2023-03-16 00:45:47 -0700

    Add slider, fade action, reset

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Etch-a-Sketch
+
+A simple browser program where mousing over elements in a grid changes the rgb value of each box. 
--- e-a-s.js
+++ e-a-s.js
@@ -1,41 +1,52 @@
 
 const gridContainer = document.querySelector('.gridContainer');
 
-let dimensions = 100;
-let ps = [];
-const stretch = parseInt(960/dimensions);
-gridContainer.style.gridTemplateColumns = (`repeat(${dimensions}, 1fr`);
-gridContainer.style.gridTemplateRows = (`repeat(${dimensions}, 1fr`);
-let colors = ['#000', '#111', '#222', '#333', '#444', '#555',
-                '#666', '#777', '#888', '#999', '#AAA', '#BBB',
-                '#CCC', '#DDD', '#EEE', '#FFF'];
-let picker = new Array(dimensions * dimensions).fill(0);
+function createGrid(dimensions){
+    // let dimensions = 100;
+    let pixel = [];
+    //stretch fits the pixel size to a fraction of the total grid size
+    const stretch = Math.round(parseInt(960/dimensions));
+    //resizeable grid
+    gridContainer.style.gridTemplateColumns = (`repeat(${dimensions}, 1fr`);
+    gridContainer.style.gridTemplateRows = (`repeat(${dimensions}, 1fr`);
+    //some hard-coded grayscale values
+    let colors = ['#000', '#111', '#222', '#333', '#444', '#555',
+                    '#666', '#777', '#888', '#999', '#AAA', '#BBB',
+                    '#CCC', '#DDD', '#EEE', '#FFF'];
+    //each pixel needs its own array for the color to
+    //incrementally change per pixel
+    let colorPerPixel = new Array(dimensions * dimensions).fill(0);
 
-
-
-for (let i = 0; i < (dimensions*dimensions); i++){
-        
-        ps[i] = document.createElement('div');
-        ps[i].classList.add('ps');
-        ps[i].style.width = stretch+'px';
-        ps[i].style.height = stretch+'px'; 
-        ps[i].addEventListener('mouseover', function(event){
-             
-            event.target.style.backgroundColor = colors[(picker[i] += 1)%16];
-            // ++picker;
-        })
-        // ps[i].addEventListener('mouseenter', function(event){
+    for (let i = 0; i < (dimensions*dimensions); i++){
             
-        //     event.target.style.backgroundColor = colors[0];
-        //     // ++picker;
-        // })
-        
-        gridContainer.appendChild(ps[i]);
-        
+        pixel[i] = document.createElement('div');
+        pixel[i].classList.add('pixel');
+        pixel[i].style.width = stretch+'px';
+        pixel[i].style.height = stretch+'px'; 
+            //a listener for each pixel
+            pixel[i].addEventListener('mouseover', function(event){       
+                event.target.style.backgroundColor = colors[(colorPerPixel[i] += 1)];
+                    })
+                    gridContainer.appendChild(pixel[i]);       
+        }
     }
 
+function deleteGrid(){
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+      }
+}
 
-// createGrid(10);
+
+var slider = document.getElementById("myRange");
+var btn = document.getElementById("myBtn");   
+     btn.addEventListener("click", function(){ deleteGrid()});
+	 btn.addEventListener("click", function(){ createGrid(slider.value)});
+
+
+
+
+createGrid(48);
     
 
 
